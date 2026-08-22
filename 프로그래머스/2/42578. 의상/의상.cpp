e2c69d71d34cbcfd180d5ffd154cad5e6fbbd19b
LEE <127584490+binedwin@users.noreply.github.com>
2026-08-22 10:22:36 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer =1;
+    unordered_map <string, int> um;
+    for(int i =0; i<clothes.size(); i++){
+        um[clothes[i][1]]++;
+    }
+    for (auto x : um) {
+        answer *= (x.second + 1);
+    }
+
+    return answer - 1;
+}
